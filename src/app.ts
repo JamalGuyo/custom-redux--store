73503ea36d3473e6ec578e3d46a00f049272bc9f
JamalGuyo <jamalguyo08@gmail.com>
2022-02-21 12:05:35 +0300

    create and register reducers

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,13 @@ const button = document.querySelector("button") as HTMLButtonElement;
 const destroy = document.querySelector(".unsubscribe") as HTMLButtonElement;
 const todoList = document.querySelector(".todos") as HTMLLIElement;
 
+// define all reducers here
+const reducers = {
+  todos: fromStore.reducer,
+};
+
 // set state with reducer and initial state
-const store = new fromStore.Store(
-  {},
-  { todos: [{ label: "Eat pizza", completed: false }] }
-);
+const store = new fromStore.Store(reducers);
 
 button.addEventListener(
   "click",
@@ -23,6 +25,7 @@ button.addEventListener(
       type: "ADD_TODO",
       payload,
     });
+    console.log(store.value);
 
     input.value = "";
   },
